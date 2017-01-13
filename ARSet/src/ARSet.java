a25be2302834560fcc8781c55136301efb808cd5
Robert Krook <oamannen@yahoo.com>
2017-01-13 20:24:39 +0100

    initial implementation, not tested at all.

--- ARSet/src/ARSet.java
+++ ARSet/src/ARSet.java
@@ -2,92 +2,118 @@
  * Created by aleksandarbabunovic on 2017-01-13.
  */
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
 /**
- * A set of Elements E that does not allow duplicates.
+ * A set of Integers that does not allow duplicates.
  */
-public class ARSet {
+public class ARSet implements Iterable<Integer>{
 
-    private LinkedList list;
-    private int capacity;
-
-    public ARSet(){
-        list = null;
-        capacity = 0;
+    public static void main(String[] args) {
     }
 
-    public boolean add(int elem){
-        if (list == null){
-            list = new LinkedList(elem,null);
-            capacity++;
-            return true;
+    /**
+     * Implementation is done through delegation.
+     */
+    private LinkedList<Integer> elems;
+
+    /**
+     * Simply initiates the LinkedList<Integer> Elems.
+     */
+    public ARSet() {
+        elems = new LinkedList<>();
+    }
+
+    /**
+     * Adds an integer to the set. If the integer is already present in the set, the set is left unchanged.
+     * @param i is the integer to be added.
+     */
+    public void add(Integer i) {
+        if(!elems.contains(i)) {
+            elems.add(i);
         }
-        LinkedList index = list;
-        while(index.next != null){
-            if(index.next.elem == elem) {
-                return false;
+    }
+
+    /**
+     * Deletes an integer from the set. If the integer is not present in the set, the set is left unchanged.
+     * @param i is the integer to be added.
+     */
+    public void delete(Integer i) {
+        elems.remove(i);
+    }
+
+    /**
+     * Creates a new set which is the union of the set it is called upon and the set that is supplied.
+     * Both the set it is called upon and the set that is supplied are left unchanged.
+     * @param set is the set that is supplied.
+     * @return a new set that is the union of the two sets.
+     */
+    public ARSet union(ARSet set) {
+        ARSet newset = new ARSet();
+        Iterator<Integer> iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Integer i = iterator.next();
+            if(!elems.contains(i)) {
+                newset.add(i);
             }
-            index = index.next;
         }
-        index.next = new LinkedList(elem,null);
-        capacity++;
-        return true;
+        iterator = this.iterator();
+        while(iterator.hasNext()) {
+            newset.add(iterator.next());
+        }
+        return newset;
     }
 
-    public int head(){
-        return list.elem;
+    /**
+     * Creates a new set which is the intersection of the set it is called upon and the set that is supplied.
+     * Both the set it is called upon and the set that is supplied are left unchanged.
+     * @param set is the set that is supplied.
+     * @return a new set that is the union of the two sets.
+     */
+    public ARSet intersection(ARSet set) {
+        ARSet newset = new ARSet();
+        Iterator<Integer> iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Integer i = iterator.next();
+            if(elems.contains(i)) {
+                newset.add(i);
+            }
+        }
+        return newset;
     }
 
-    public ARSet tail(){
-        ARSet res = new ARSet();
-        res.capacity = capacity-1;
-        LinkedList index = list;
-        res.list = new LinkedList(index.next.elem, null);
-        LinkedList resIndex = res.list;
-        index = index.next;
-        while(index.next != null){
-            resIndex.next = new LinkedList(index.next.elem, null);
-            resIndex = resIndex.next;
-            index = index.next;
-        }
-        return res;
+    /* Boiler-plate stuff to be able to iterate over the set. Not of interest to students. */
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new ARSetIterator(this);
     }
 
-    public boolean remove(int elem){
-        LinkedList index = list;
-        if(index.elem == elem){
-            list = list.next;
-            capacity--;
-            return true;
+    private class ARSetIterator implements Iterator<Integer> {
+
+        private ARSet set;
+        private int current;
+
+        public ARSetIterator(ARSet set) {
+            this.set = set;
+            current = 0;
         }
-        while(index.next != null){
-            if(index.next.elem == elem){
-                index.next = index.next.next;
-                capacity--;
-                return true;
+
+        @Override
+        public boolean hasNext() {
+            return current < set.elems.size() - 1;
+        }
+
+        @Override
+        public Integer next() {
+            if(hasNext()) {
+                return set.elems.get(current++);
             } else {
-                index = index.next;
+                throw new NoSuchElementException();
             }
-        }
-        return false;
-    }
-
-    public int[] toArray(){
-        int res[] = new int[capacity];
-        LinkedList index = list;
-        for(int i = 0; i < capacity; i++){
-            res[i] = index.elem;
-            index = index.next;
-        }
-        return res;
-    }
-
-    private class LinkedList {
-        private int elem;
-        private LinkedList next;
-
-        private LinkedList(int elem, LinkedList next){
-            this.elem = elem;
-            this.next = next;
         }
     }
 }
